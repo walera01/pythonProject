494656a2408b8e082ddf5b2aeb636d9ae5d8cc51
Владислав <wladislaw1004@gmail.com>
2022-03-30 14:29:40 +0300

    создание адресов

--- main2.py
+++ main2.py
@@ -1,1 +1,12 @@
-print(True, True, True == (True, True, True))
+from tkinter import *
+
+def Call():
+    a=Label(window, text="Hello world")
+    a.place(x = 30, y = 50)
+    button["bg"] = "blue"
+    button["fg"] = "white"
+window=Tk()
+window.geometry("200x110")
+button=Button(text="Touth me", command=Call)
+button.place(x = 30, y = 20, width = 120, height = 25)
+window.mainloop()
